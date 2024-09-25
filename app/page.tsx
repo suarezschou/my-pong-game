406b4a01ffcd9816576ed087c86794901faf24a8
André Suárez Schou <andre_suarez_schou@hotmail.com>
2024-09-25 21:15:00 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import PongGame from '../components/pong-game'
 
 export default function Home() {
   return (
-        <div className='bg-black'>
+        <div className='bg-black justify-center'>
           <PongGame />
         </div>
   )}
